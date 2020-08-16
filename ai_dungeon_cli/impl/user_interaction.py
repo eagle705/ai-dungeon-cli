6fcc7810c3773cab26e382dc06eb4348fed1ab18
Nako Sung <deif79@gmail.com>
2020-08-16 14:38:36 +0900

    Remove unnecessary codes

--- ai_dungeon_cli/impl/user_interaction.py
+++ ai_dungeon_cli/impl/user_interaction.py
@@ -52,45 +52,11 @@ class TermIo(UserIo):
         terminal_size = shutil.get_terminal_size((80, 20))
         return terminal_size.columns
 
-    def display_splash(self):
-        filename = os.path.dirname(os.path.realpath(__file__))
-        locale = None
-        term = None
-        if "LC_ALL" in os.environ:
-            locale = os.environ["LC_ALL"]
-        if "TERM" in os.environ:
-            term = os.environ["TERM"]
-
-        if locale == "C" or (term and term.startswith("vt")):
-            filename += "/../res/opening-ascii.txt"
-        else:
-            filename += "/../res/opening-utf8.txt"
-
-        with open(filename, "r", encoding="utf8") as splash_image:
-            print(splash_image.read())
-
     def clear(self):
         if os.name == "nt":
             _ = os.system("cls")
         else:
             _ = os.system("clear")
-
-
-# -------------------------------------------------------------------------
-# IMPLEM: SLOW TYPING EFFECT
-
-class TermIoSlowStory(TermIo):
-    def __init__(self, prompt: str = ''):
-        sys.stdout = Unbuffered(sys.stdout)
-        super().__init__(prompt)
-
-    def handle_story_output(self, text: str):
-        for line in textwrap.wrap(text, self.get_width()):
-            for letter in line:
-                print(letter, end='')
-                sleep(randint(2, 10)*0.005)
-                print()
-                print()
 
 
 # allow unbuffered output for slow typing effect
